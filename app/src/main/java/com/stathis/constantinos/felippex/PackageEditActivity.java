85ac95620399125e0eaeea681806ca1b74b1803d
Constantinos Stathis <constantinos-@hotmail.com>
2018-04-13 00:55:40 +0300

    Package photo capture created

--- app/src/main/java/com/stathis/constantinos/felippex/PackageEditActivity.java
+++ app/src/main/java/com/stathis/constantinos/felippex/PackageEditActivity.java
@@ -1,13 +1,118 @@
 package com.stathis.constantinos.felippex;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PackageEditActivity extends AppCompatActivity {
+
+    private final String APP_TAG="FelippEx";
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private ImageView packageImageView;
+    String mCurrentPhotoPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_package_edit);
+
+        packageImageView = (ImageView) findViewById(R.id.package_image_placeholder);
+    }
+
+    public void onRequestPhoto(View v) {
+        dispatchTakePictureIntent();
+    }
+
+    // Following Snippets Source: https://developer.android.com/training/camera/photobasics.html
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Log.e(APP_TAG,"Error occurred while creating the File");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            setPic();
+        }
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = packageImageView.getWidth();
+        int targetH = packageImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap imageOutput = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        packageImageView.setImageBitmap(prepareImageForPreview(imageOutput));
+    }
+
+    private Bitmap prepareImageForPreview(Bitmap image) {
+        Matrix matrix = new Matrix();
+        matrix.preRotate(90);
+        return Bitmap.createBitmap(image, 0,0,image.getWidth(),image.getHeight(), matrix, true);
     }
 }
